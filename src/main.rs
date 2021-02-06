45b04969f395cc5c483725d695821418e75289fe
AndrewPiroli <45859969+AndrewPiroli@users.noreply.github.com>
2021-02-05 21:53:48 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::path::Path;
 
 fn printhelp(name: String){
     println!("Usage");
@@ -8,13 +9,13 @@ fn printhelp(name: String){
 fn main(){
     let args: Vec<String> = env::args().collect();
     // Arguments: input output step heartbeat
-    // input and output are files
+    // input and output are Paths
     // step and heartbeat should be integers
     if args.len() != 5 {
         printhelp(args[0].clone());
         return;
     }
-    let (infile, outfile): (String, String) = (args[1].clone(), args[2].clone());
+    let (infile, outfile): (Path, Path) = (Path::new(args[1]), Path::new(args[2]));
     let (step, heartbeat): (u64, u64) = (args[3].parse().unwrap(), args[4].parse().unwrap());
     println!("infile:{} outfile:{} step:{} heartbeat:{}", infile, outfile, step, heartbeat);
     println!("Hello rust!");
