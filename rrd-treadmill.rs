ee796fc58081465d3d33311bfbd4f356e040b4a5
AndrewPiroli <45859969+AndrewPiroli@users.noreply.github.com>
2021-02-05 20:41:42 -0500

    Try this

--- rrd-treadmill.rs
+++ rrd-treadmill.rs
@@ -1,5 +1,21 @@
-use std::io;
+use std::env;
+
+fn printhelp(name: String){
+    println!("Usage")
+    println!("{} input_file output_file step heartbeat", name)
+}
 
 fn main(){
+    let args: Vec<String> = env::args().collect();
+    // Arguments: input output step heartbeat
+    // input and output are files
+    // step and heartbeat should be integers
+    if (args.len() != 5){
+        printhelp();
+        return;
+    }
+    let (infile, outfile): (String, String) = (&args[1], &args[2]);
+    let (step, heartbeat): (u64, u64) = (&args[3].parse().unwrap(), &args[4].parse().unwrap());
+    println!("infile:{} outfile:{} step:{} heartbeat:{}", infile, outfile, step, heartbeat)
     println!("Hello rust!");
 }
