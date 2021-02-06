ee4e19158c2c6436dc8dec7714f5e02215aacc81
AndrewPiroli <45859969+AndrewPiroli@users.noreply.github.com>
2021-02-05 20:43:44 -0500

    try again

--- rrd-treadmill.rs
+++ rrd-treadmill.rs
@@ -1,8 +1,8 @@
 use std::env;
 
 fn printhelp(name: String){
-    println!("Usage")
-    println!("{} input_file output_file step heartbeat", name)
+    println!("Usage");
+    println!("{} input_file output_file step heartbeat", name);
 }
 
 fn main(){
@@ -10,12 +10,12 @@ fn main(){
     // Arguments: input output step heartbeat
     // input and output are files
     // step and heartbeat should be integers
-    if (args.len() != 5){
-        printhelp();
+    if args.len() != 5 {
+        printhelp(args[0]);
         return;
     }
-    let (infile, outfile): (String, String) = (&args[1], &args[2]);
-    let (step, heartbeat): (u64, u64) = (&args[3].parse().unwrap(), &args[4].parse().unwrap());
+    let (infile, outfile): (String, String) = (args[1], args[2]);
+    let (step, heartbeat): (u64, u64) = (args[3].parse().unwrap(), args[4].parse().unwrap());
     println!("infile:{} outfile:{} step:{} heartbeat:{}", infile, outfile, step, heartbeat)
     println!("Hello rust!");
 }
