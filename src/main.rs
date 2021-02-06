7b8ebe12c80dc02de94c7a3adca73beba2ec562b
AndrewPiroli <45859969+AndrewPiroli@users.noreply.github.com>
2021-02-05 21:54:37 -0500

    test

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main(){
         printhelp(args[0].clone());
         return;
     }
-    let (infile, outfile): (Path, Path) = (Path::new(args[1]), Path::new(args[2]));
+    let (infile, outfile): (&Path, &Path) = (Path::new(args[1]), Path::new(args[2]));
     let (step, heartbeat): (u64, u64) = (args[3].parse().unwrap(), args[4].parse().unwrap());
     println!("infile:{} outfile:{} step:{} heartbeat:{}", infile, outfile, step, heartbeat);
     println!("Hello rust!");
