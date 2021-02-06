9b168d9e277c8750a5e94b2a3bbbd309248e04ad
AndrewPiroli <45859969+AndrewPiroli@users.noreply.github.com>
2021-02-05 22:14:07 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::path::Path;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn printhelp(name: String){
     println!("Usage");
@@ -15,8 +17,16 @@ fn main(){
         printhelp(args[0].clone());
         return;
     }
-    let (infile, outfile): (&Path, &Path) = (Path::new(&args[1]), Path::new(&args[2]));
+    let (infile_p, outfile_p): (&Path, &Path) = (Path::new(&args[1]), Path::new(&args[2]));
     let (step, heartbeat): (u64, u64) = (args[3].parse().unwrap(), args[4].parse().unwrap());
-    println!("infile:{} outfile:{} step:{} heartbeat:{}", infile.display(), outfile.display(), step, heartbeat);
-    println!("Hello rust!");
+    println!("infile:{} outfile:{} step:{} heartbeat:{}", infile_p.display(), outfile_p.display(), step, heartbeat);
+    let infile_ob = File::open(infile_p).unwrap();
+    let infile_reader = BufReader::new(infile_ob);
+    let infile_lines = Vec<String>::new();
+    for line in infile_reader.lines() {
+        infile_lines.append(line.unwrap());
+    }
+    for (index, test) in infile_lines.enumerate() {
+        println("{} {}", index, test);
+    }
 }
